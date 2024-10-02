7cc3fc0a227d3376037f9ecfc3c7f590141de9ef
deisyh <167341237+deisyh@users.noreply.github.com>
2024-10-02 17:22:49 +0000

    api

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,5 +1,7 @@
+import { getCSS } from "./common.js";
+
 async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
@@ -9,14 +11,22 @@ async function quantidadeUsuarios() {
         {
            x: nomeDasRedes,
            y: quantidadeUsuarios,
-           type: 'bar'
+           type: 'bar',
+           marker: {
+               color: getCSS('--primary-color')
+           }
         }
     ]
     
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+    }
+
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuarios()
