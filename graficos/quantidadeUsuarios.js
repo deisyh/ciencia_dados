ad419bc6c6f24a4184fc905f0f3c0697deab645e
deisyh <167341237+deisyh@users.noreply.github.com>
2024-10-09 17:22:35 +0000

    redes

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -51,10 +51,6 @@ async function quantidadeUsuarios() {
         
         }
 
-
-        
-
-
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
