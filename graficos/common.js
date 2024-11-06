13d8238b2c97dea982939565145c6c8c2a2bd560
deisyh <167341237+deisyh@users.noreply.github.com>
2024-11-06 17:23:27 +0000

    redes sociais

--- graficos/common.js
+++ graficos/common.js
@@ -8,4 +8,23 @@ const getCSS = (variavel) => {
     family: getCSS('--font')
   }
   
-  export { getCSS, tickConfig }
+  function criarGrafico(data, layout) {
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    const config = {
+      responsive: true,
+      displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config) 
+  }
+
+  function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`;
+    container.appendChild(paragrafo)
+  }
+
+  export { getCSS, tickConfig, criarGrafico, incluirTexto }
--- graficos/redesFavoritasMundo.js
+++ graficos/redesFavoritasMundo.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js"
+import { getCSS, criarGrafico, incluirTexto } from "./common.js"
 
 async function redesFavoritasMundo() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
@@ -19,6 +19,7 @@ async function redesFavoritasMundo() {
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
+        height: 700,
         title: {
             text: 'Redes sociais que os usuários mais gostam',
              x: 0,
@@ -28,10 +29,17 @@ async function redesFavoritasMundo() {
                 size: 30
             }    
         },
-          
+        legend: {
+            font: {
+                color: getCSS('--primary-color'),
+                size: 16
+            }
+        }
     }
 
-    
+    criarGrafico(data, layout)    
+  
+    incluirTexto(``) 
 }
-
+ 
 redesFavoritasMundo()
